649a72ebf4c7a775ea1caefd2c15c93c7bf4f9a5
CRISTOBALRODRI2 <>
2022-04-08 15:46:23 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -83,11 +83,28 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-
-    return NULL;
+  
+  for(long i=0; i<(map->capacity); i++){
+    if(map->hashArray[i] != NULL ){
+      if(map->hashArray[i]->value != NULL){
+          map->current = i;
+          return map->hashArray[map->current]->value;
+      }
+    }
+  }
+  return NULL;
 }
 
 Pair * nextMap(HashMap * map) {
-
+ 
+    for( long i= (map->current + 1); i<(mapa->capacity); i++){
+      if( map->hashArray[i] != NULL){
+        if(map->hashArray[i]->value != NULL){
+          map->current = i;
+          return map->hashArray[map->current]->value;
+            }
+        }
+    }
     return NULL;
+  
 }
