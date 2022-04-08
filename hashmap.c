e4af211a3a0ebe327936293e99ce896ef9888a4a
CRISTOBALRODRI2 <>
2022-04-08 01:25:34 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -55,11 +55,11 @@ HashMap * createMap(long capacity) {
     HashMap *m=(HashMap*)malloc(sizeof(HashMap));
     m->size=0;
     m->capacity=capacity;
-    
+    m->buckets=(Pair**)malloc(sizeof(Pair*));
     int i;
 
     for (i= 0; i<m->size; i++)
-      m->buckets[i]= NULL;
+      m->buckets[i]=NULL;
     return m;
 }
 
